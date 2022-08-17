6cadeb7d8c73883364dae5d396eb5f024fc557e5
Akloni1 <61523151+Akloni1@users.noreply.github.com>
2022-08-17 08:07:44 +0300

    Added project

---
+++ BD/BD/CreateBD.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BD
+{
+    public class CreateBD
+    {
+        private static SqlConnection myConn;
+        private static SqlConnection myConn1;
+        private static String createBD;
+        
+        public CreateBD(string server, string name)
+        {
+            myConn = new SqlConnection($"Server={server};Trusted_connection=True");
+            myConn1 = new SqlConnection($"Server={server};Database={name};Trusted_connection=True");
+            createBD = $"CREATE DATABASE {name}";
+        }
+        
+        
+       public void CreateBd()
+       {
+           SqlCommand myCommand = new SqlCommand(createBD, myConn);
+           try
+           {
+               myConn.Open();
+               myCommand.ExecuteNonQuery();
+           }
+
+           catch (Exception ex)
+           {
+              
+               Console.WriteLine("Error" + ex.Message);
+           }
+           finally
+           {
+               if (myConn.State == ConnectionState.Open)
+               {
+                   myConn.Close();
+               }
+           }
+       }
+
+
+
+       public void CreateTableCust()
+       {
+           SqlCommand command = new SqlCommand();
+           command.CommandText = "CREATE TABLE Customers (Id INT PRIMARY KEY IDENTITY, Name NVARCHAR(100) NOT NULL)";
+           command.Connection = myConn1;
+           try
+           {
+               myConn1.Open();
+               command.ExecuteNonQuery();
+           }
+           catch (Exception ex)
+           {
+               Console.WriteLine("Error" + ex.Message);
+           }
+           finally
+           {
+               if (myConn1.State == ConnectionState.Open)
+               {
+                   myConn1.Close();
+               }
+           }
+       }
+       
+       
+       public void CreateTableOrders()
+       {
+           SqlCommand command = new SqlCommand();
+           command.CommandText = "CREATE TABLE Orders (Id INT PRIMARY KEY IDENTITY, CustomerId INT NOT NULL)";
+           command.Connection = myConn1;
+           try
+           {
+               myConn1.Open();
+               command.ExecuteNonQuery();
+           }
+           catch (Exception ex)
+           {
+               Console.WriteLine("Error" + ex.Message);
+           }
+           finally
+           {
+               if (myConn1.State == ConnectionState.Open)
+               {
+                   myConn1.Close();
+               }
+           }
+       }
+
+       public void AddCust()
+       {
+           string sqlExpression = "INSERT INTO Customers (Name) VALUES ('Max'), ('Pavel'), ('Ivan'), ('Leonid')";
+           SqlCommand command = new SqlCommand(sqlExpression, myConn1);
+           try
+           {
+               myConn1.Open();
+               command.ExecuteNonQuery();
+           }
+           catch (Exception ex)
+           {
+               Console.WriteLine("Error" + ex.Message);
+           }
+           finally
+           {
+               if (myConn1.State == ConnectionState.Open)
+               {
+                   myConn1.Close();
+               }
+           }
+       }
+
+       
+       
+       public void AddOrders()
+       {
+           string sqlExpression = "INSERT INTO Orders (CustomerId) VALUES (2), (4)";
+           SqlCommand command = new SqlCommand(sqlExpression, myConn1);
+           try
+           {
+               myConn1.Open();
+               command.ExecuteNonQuery();
+           }
+           catch (Exception ex)
+           {
+               Console.WriteLine("Error" + ex.Message);
+           }
+           finally
+           {
+               if (myConn1.State == ConnectionState.Open)
+               {
+                   myConn1.Close();
+               }
+           }
+       }
+    }
+    }
---
+++ BD/BD/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BD
+{
+    class Program
+    {
+        
+      
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сервер для базы данных:");
+            string server = Console.ReadLine();
+            Console.WriteLine("Введите название базы данных:");
+            string name = Console.ReadLine();
+
+            CreateBD createBd = new CreateBD(server, name);
+              createBd.CreateBd();
+               createBd.CreateTableCust();
+              createBd.CreateTableOrders();
+              createBd.AddCust();
+             createBd.AddOrders();
+            string connectionString = $"Server={server};Database={name};Trusted_connection=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            try
+            {
+                string sqlCommand = "SELECT Name as Customers FROM Customers " +
+                    "LEFT JOIN Orders ON Customers.Id= Orders.CustomerId " +
+                    "WHERE Orders.CustomerId is null";
+
+
+                SqlCommand command = new SqlCommand(sqlCommand, connection);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        Console.WriteLine(reader[i] + "\t");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error" + ex.Message);
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            } 
+        }
+    }
+}
